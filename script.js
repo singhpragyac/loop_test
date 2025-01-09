374c1e9d0f05455ac46ec64824c55146671b392d
Pragya Singh <singhpragyac@gmail.com>
2025-01-09 23:54:18 +0530

    fixed indetation

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 let x = 0;
 var y = 0;
-function click_to_move()  {
-   // console.log("helllo");
+function click_to_move() {
+    // console.log("helllo");
     let ela = document.getElementById("my_id");
-    if( x <= 500 && y == 0) {
-        x+=110;
+    if (x <= 500 && y == 0) {
+        x += 110;
         ela.style.left = x + "px";
         console.log("mid");
-    } else if(x >= 500 && y <= 300) {
+    } else if (x >= 500 && y <= 300) {
         y += 60;
         ela.style.top = y + "px";
         console.log("middd");
-    } else if(y >= 300 && x >= 0) {
-        x -=70;
+    } else if (y >= 300 && x >= 0) {
+        x -= 70;
         ela.style.left = x + "px";
         console.log("last");
-    } else if(x <= 0 && y >= 0){
+    } else if (x <= 0 && y >= 0) {
         y -= 60;
         ela.style.top = y + "px";
         console.log("hhhhhh");
