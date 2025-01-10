92ddba9d62b7609508e568c623d9b4b7b434497c
Pragya Singh <singhpragyac@gmail.com>
2025-01-10 14:27:42 +0530

    setting interval

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var y = 0;
 function click_to_move() {
     // console.log("helllo");
     let ela = document.getElementById("my_id");
+    const myinterval = setInterval(frame,1000)
+   function frame() {
     if (x <= 500 && y == 0) {
         x += 110;
         ela.style.left = x + "px";
@@ -13,11 +15,18 @@ function click_to_move() {
         console.log("middd");
     } else if (y >= 300 && x >= 0) {
         x -= 70;
-        ela.style.left = x + "px";
+        ela.style.left = x + "px"; 
         console.log("last");
     } else if (x <= 0 && y >= 0) {
         y -= 60;
         ela.style.top = y + "px";
         console.log("hhhhhh");
+    } else if (x == 0 && y == 0) {
+        clearInterval(myinterval);
+        console.log("stop");
     }
+        
+    }
+    
+   
 }
